085d6f4bd69b277e3c626f9f0566e88f7ace6786
Marcel Kuipers <marcel96@outlook.com>
2018-10-04 11:02:30 +0200

    read file and added description

---
+++ ambulanceLocationProblem.py
@@ -0,0 +1,15 @@
+import numpy as np
+from pulp import *
+#from numpy import *
+
+# Open data set file
+F = open('./Ambulance instances/Region14.txt','r') 
+
+[n,p] =[range(0, int(s)) for s in F.readline().split()]
+
+w = [int(s) for s in F.readline().split()]
+
+d = np.empty([len(n),len(n)])
+
+for i in n:
+    d[i] = [int(s) for s in F.readline().split()]
