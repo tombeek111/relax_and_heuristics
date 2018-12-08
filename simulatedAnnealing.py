ddbd7c8645cd4daddaf6c10ef3aff8c74f01c2b5
Marcel Kuipers <marcel96@outlook.com>
2018-12-08 16:18:13 +0100

    Added simulated annealing for branch and cut

--- simulatedAnnealing.py
+++ simulatedAnnealing.py
@@ -61,6 +61,53 @@ class SAInstance():
             solution = bestCandidate
         return solution
 
+    def SA(self, initialSolution, M,stopafter=100):
+        currentSolution = initialSolution
+        bestSolution = currentSolution
+        bestObjective = currentSolution.objective()
+        currentObjective = bestObjective
+        temperature = self.initialT()
+        lastAccept = 0
+        
+        
+        for t in range(1, M):
+            if t-lastAccept > stopafter:
+                break
+            
+            nextSolution = currentSolution.mutateSolution()
+            nextObjective = nextSolution.objective()
+            #If the new solution is better, replace the current solution
+            if nextObjective < currentObjective:
+                
+                currentSolution = nextSolution
+                currentObjective = nextObjective
+                lastAccept = t
+            else:
+                temperature = temperature * 0.9
+                accProb = acceptanceProbability(currentObjective, nextObjective, temperature)
+#                print(temperature,accProb)
+                if random.random() <= accProb:
+                    currentSolution = nextSolution
+                    currentObjective = nextObjective
+                    lastAccept = t
+           
+            if currentObjective < bestObjective:
+                bestObjective = currentObjective
+                bestSolution = currentSolution
+        return bestSolution
+    
+    def initialT(self):
+        largestSum = 0
+        for i in self.N:
+            weightedSum= 0
+            for j in self.N:
+                if i != j:
+                    weightedSum += self.w[j] * self.D[j][i]
+            if weightedSum > largestSum:
+                largestSum = weightedSum
+        return largestSum * 2.5
+
+
 class SASolution():
     def __init__(self, solution, instance):
         self.solution = solution
@@ -82,26 +129,26 @@ class SASolution():
                 totalWeight += self.w[i] * minDistance
         return totalWeight
 
-#Mutate solution
-def mutateSolution(solution):
-    found = False
-    while not found:
-        newCity = random.randint(0,len(N))
-        found = newCity in solution
-    replaceCityIndex = random.randint(0,p - 1)
-    newSolution = solution.copy()
-    newSolution[replaceCityIndex] = newCity
-    return newSolution
+    #Mutate solution
+    def mutateSolution(self):
+        found = False
+        while not found:
+            newCity = random.randint(0,len(self.N) -1)
+            found = newCity not in self.solution
+        replaceCityIndex = random.randint(0,self.p - 1)
+        newSolution = self.solution.copy()
+        newSolution[replaceCityIndex] = newCity
+        return SASolution(newSolution, self.instance)
 
 
-def neighborhoodIndex(solution, index):
-    neighborhoodIndices = list(filter(lambda x: x not in solution, N))
-    neighborhood = []
-    for i in neighborhoodIndices:
-        neigbhorhoodSolution = solution.copy()
-        neigbhorhoodSolution[index] = i
-        neighborhood.append(neigbhorhoodSolution)
-    return neighborhood
+    def neighborhoodIndex(self,solution, index):
+        neighborhoodIndices = list(filter(lambda x: x not in solution.solution, self.N))
+        neighborhood = []
+        for i in neighborhoodIndices:
+            neigbhorhoodSolution = solution.solution.copy()
+            neigbhorhoodSolution[index] = i
+            neighborhood.append(neigbhorhoodSolution)
+        return SASolution(neighborhood, self.instance)
 
 def neighborhood(solution):
     neighborhood = map(lambda x: neighborhoodIndex(solution,x), range(0,len(solution)))   
@@ -121,91 +168,47 @@ def neighborhoodSearch(solution):
     return solution
 
 
-def mutateSolution(solution):
-    replaceCityIndex = random.randint(0,len(solution) - 1)
-    neighborhood = neighborhoodIndex(solution, replaceCityIndex)
-    return bestCandidate(neighborhood)
+#def mutateSolution(solution):
+#    replaceCityIndex = random.randint(0,len(solution.solution) - 1)
+#    neighborhood = neighborhoodIndex(solution, replaceCityIndex)
+#    return bestCandidate(neighborhood)
+#
+#def T(t, initialT):
+#    return (M/t)
 
-def T(t, initialT):
-    return (M/t)
-
-def initialT():
-    largestSum = 0
-    for i in N:
-        weightedSum= 0
-        for j in N:
-            if i != j:
-                weightedSum += w[j] * D[j][i]
-        if weightedSum > largestSum:
-            largestSum = weightedSum
-    return largestSum * 2.5
 
 def acceptanceProbability(currentObjective, nextObjective, temperature):
     coefficient =  -(abs(currentObjective - nextObjective)) / temperature
     return pow(math.e,coefficient)
 
-def greedy(p):
-    solution = []
-    for i in range(0,p):
-        bestObjectiveCandidate = math.inf
-        bestCandidate = []
-        for j in range(1, len(N)):
-            if j not in solution:
-                candidate = solution + [j]
-                objectiveCandidate = objective(candidate)
-                if objectiveCandidate < bestObjectiveCandidate:
-                    bestObjectiveCandidate = objectiveCandidate
-                    bestCandidate = candidate
-        solution = bestCandidate
-    return solution
+#def greedy(p):
+#    solution = []
+#    for i in range(0,p):
+#        bestObjectiveCandidate = math.inf
+#        bestCandidate = []
+#        for j in range(1, len(N)):
+#            if j not in solution:
+#                candidate = solution + [j]
+#                objectiveCandidate = objective(candidate)
+#                if objectiveCandidate < bestObjectiveCandidate:
+#                    bestObjectiveCandidate = objectiveCandidate
+#                    bestCandidate = candidate
+#        solution = bestCandidate
+#    return solution
 
-def SA(initialSolution, M,stopafter=100):
-    currentSolution = initialSolution
-    bestSolution = currentSolution
-    bestObjective = objective(currentSolution)
-    currentObjective = bestObjective
-    temperature = initialT()
-    lastAccept = 0
-    
-    
-    for t in range(1, M):
-        if t-lastAccept > stopafter:
-            break
-        
-        nextSolution = mutateSolution(currentSolution)
-        nextObjective = objective(nextSolution)
-        #If the new solution is better, replace the current solution
-        if nextObjective < currentObjective:
-            
-            currentSolution = nextSolution
-            currentObjective = nextObjective
-            lastAccept = t
-        else:
-            temperature = temperature * 0.9
-            accProb = acceptanceProbability(currentObjective, nextObjective, temperature)
-            #print(temperature,accProb)
-            if random.random() <= accProb:
-                currentSolution = nextSolution
-                currentObjective = nextObjective
-                lastAccept = t
-       
-        if currentObjective < bestObjective:
-            bestObjective = currentObjective
-            bestSolution = currentSolution
-    return bestSolution
-    
 
-def compute_ub_sa(problem):
-    N = problem.solver.problem.N
-    w = problem.solver.problem.w
-    D = problem.solver.problem.D
-    p = problem.solver.problem.p
-    instance = SAInstance(N,w,D,p)
-    solution = instance.randomSolution([1,3],[0,4])
-    solution = instance.greedy()
-    print(problem.y_one)
-    print(problem.y_zero)
-    return solution.objective()
+
+#def compute_ub_sa(problem):
+#    N = problem.solver.problem.N
+#    w = problem.solver.problem.w
+#    D = problem.solver.problem.D
+#    p = problem.solver.problem.p
+#    instance = SAInstance(N,w,D,p)
+#    solution = instance.randomSolution([1,3],[0,4])
+#    solution = instance.greedy()
+#    print(problem.y_one)
+#    print(problem.y_zero)
+#    return solution.objective()
 #
 #M = 100
 #initialSolution = neighborhoodSearch(randomSolution())
